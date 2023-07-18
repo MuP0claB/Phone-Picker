db25db407855723318b6b027308f60dfa818af7a
MuP0claB <miroslav.ivanoff@gmail.com>
2023-07-18 17:55:56 +0300

    Clear rendering logic

--- src/context/shop-context.js
+++ src/context/shop-context.js
@@ -14,6 +14,17 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = PRODUCTS.find((p) => p.id === +item);
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
+
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
   };
@@ -22,11 +33,17 @@ export const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-const updateCartItemCount = (newAmount, itemId) => {
-   setCartItems(prev => ({ ...prev, [itemId]: newAmount}));
-};
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
 
-  const contextValue = { cartItems, addToCart, removeFromCart, updateCartItemCount };
+  const contextValue = {
+    getTotalCartAmount,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    updateCartItemCount,
+  };
 
   return (
     <ShopContext.Provider value={contextValue}>
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
 import ShopContext from "../../context/shop-context";
@@ -6,22 +8,36 @@ import ShopContext from "../../context/shop-context";
 import "./Cart.css";
 
 const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { getTotalCartAmount, cartItems } = useContext(ShopContext);
+  const navigate = useNavigate();
+
+  const totalAmount = getTotalCartAmount();
 
   return (
     <div className="cart">
-      <div>
-        <h1>
-          <b>Your Cart Items</b>
-        </h1>
-      </div>
-      <div className="cartItems">
-        {PRODUCTS.map((product) => {
-          if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />;
-          }
-        })}
-      </div>
+      {totalAmount > 0 ? (
+        <>
+          <div>
+            <h1>
+              <b>Your Cart Items</b>
+            </h1>
+          </div>
+          <div className="cartItems">
+            {PRODUCTS.map((product) => {
+              if (cartItems[product.id] !== 0) {
+                return <CartItem data={product} />;
+              }
+            })}
+          </div>
+          <div className="checkout">
+            <p> Subtotal: $ {totalAmount} </p>
+            <button onClick={() => navigate("/")}> Continue Shopping </button>
+            <button> Checkout </button>
+          </div>
+        </>
+      ) : (
+        <h1> Your Cart Is Empty </h1>
+      )}
     </div>
   );
 };
