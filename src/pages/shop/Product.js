8b2c7c64b7e5bf8e9d6e711bfb56d43adee603a8
MuP0claB <miroslav.ivanoff@gmail.com>
2023-07-18 12:21:26 +0300

    Remove the backgroung-color of body element

--- src/pages/shop/Product.js
+++ src/pages/shop/Product.js
@@ -1,7 +1,17 @@
 const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
 
-  return <div>product</div>;
+  return (
+    <div className="product">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>${price}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
